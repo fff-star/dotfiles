7d40f14d34ad7990a6d767a3cd5079203c2bf419
fff <2750356943@qq.com>
2025-12-13 20:27:14 +0800

    update plug init add fzf remove ctrlp

---
+++ nvim/lua/plugins/fzf-nvim.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap("n", "<C-f>", ":Rg<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Files<CR>", { noremap = true, silent = true })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,7 +4,6 @@ Plug 'tpope/vim-sensible'
 Plug 'mileszs/ack.vim'
 Plug 'mikavilpas/yazi.nvim'
 Plug 'jiangmiao/auto-pairs'
-Plug 'ctrlpvim/ctrlp.vim'
 Plug 'vim-airline/vim-airline'
 Plug 'easymotion/vim-easymotion'
 Plug 'nvim-treesitter/nvim-treesitter'
@@ -12,9 +11,11 @@ Plug 'morhetz/gruvbox'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'junegunn/fzf.vim'
 call plug#end()
 ]]
 require("plugins.yazi")
 require("plugins.md-preview")
 require("plugins.tree-sitter")
 require("plugins.coc-nvim")
+require("plugins.fzf-nvim")
